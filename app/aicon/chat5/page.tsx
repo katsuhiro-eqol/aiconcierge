f51be1fe136cc22d1c2b4c3ca253fe35ddd07ac8
katsuhiro-eqol <katsuhiro.yamashita.sr01@gmail.com>
2026-01-29 18:30:54 +0900

    20260129

--- app/aicon/chat5/page.tsx
+++ app/aicon/chat5/page.tsx
@@ -990,7 +990,7 @@ export default function Aicon() {
             </div>
             <div className="flex-none w-full max-w-96 style={{ 
                 minHeight: '18%', 
-                paddingBottom: isSafari ? '60px' : '0px'
+                paddingBottom: isSafari ? '60px' : '60px'
             }}">
             <div className="mt-2">
             <textarea className="block w-5/6 max-w-96 mx-auto mb-2 px-2 py-2 text-base"
